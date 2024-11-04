ef39a2f025ebe5f1300417b04bdc0f834f4c9422
Tobias <131580599+KronnozKRS@users.noreply.github.com>
2024-11-04 13:29:52 -0300

    Update MouseClicked.js

--- MouseClicked.js
+++ MouseClicked.js
@@ -1,12 +1,11 @@
 function mouseClicked() {
-  
+
   if (muerte) {
     if (mouseX > 160 && mouseX < 560 && mouseY > 405 && mouseY < 455) {
       decision = false;
       muerte = false;
       ramaPos = 6;
       rama = 3;
-
     } else if (mouseX > 160 && mouseX < 560 && mouseY > 330 && mouseY < 380) {
       rama = 0;
       ramaPos = 0;
@@ -21,8 +20,66 @@ function mouseClicked() {
   if (decision && ramaPos === 2 && rama === 0) {
     if (botonNo()) {
       muerte = true;
-    } if (botonSi()) {
+    }
+    if (botonSi()) {
       ramaPos = 3;
+      decision = false;
+    }
+  }
+
+
+  if (decision && ramaPos === 6 && rama === 0) {
+    if (botonNo()) {
+      decision = false;
+      ramaPos = 0;
+      rama = 1;
+    } else if (botonSi()) {
+      ramaPos++;
+    }
+  }
+
+
+  if (decision && ramaPos === 9 && rama === 0) {
+    if (botonNo()) {
+      ramaPos = 0;
+      rama = 4;
+    } else if (botonSi()) {
+      ramaPos++;
+      decision = false;
+    }
+  }
+
+  if (decision && ramaPos === 13 && rama === 0) {
+    if (botonNo()) {
+      rama = 7;
+      ramaPos = 0;
+      decision = false;
+    } else if (botonSi()) {
+      ramaPos++;
+      decision = false;
+    }
+  }
+
+
+  if (ramaPos === 17 && rama === 0) {
+    if (botonNo()) {
+      ramaPos++;
+      decision = false;
+    } else if (botonSi()) {
+      decision = false;
+      rama = 2;
+      ramaPos = 3;
+    }
+  }
+
+
+  if (decision && ramaPos === 24 && rama === 0) {
+    if (botonNo()) {
+      ramaPos++;
+      decision = false;
+    } else if (botonSi()) {
+      rama = 8;
+      ramaPos = 0;
       decision = false;
     }
   }
@@ -38,35 +95,13 @@ function mouseClicked() {
     }
   }
 
-  if (decision && ramaPos === 24 && rama === 0) {
+  if (decision && ramaPos === 3 && rama === 1) {
     if (botonNo()) {
-      ramaPos++;
       decision = false;
-    } else if (botonSi()) {
-      rama = 8;
       ramaPos = 0;
-      decision = false;
-    }
-  }
-
-  if (decision && ramaPos === 9 && rama === 0) {
-    if (botonNo()) {
-      ramaPos = 0;
-      rama = 4;
+      rama= 2;
     } else if (botonSi()) {
       ramaPos++;
-      decision = false;
-    }
-  }
-
-  if (ramaPos === 17 && rama === 0) {
-    if (botonNo()) {
-      ramaPos++;
-      decision = false;
-    } else if (botonSi()) {
-      decision = false;
-      rama = 2;
-      ramaPos = 3;
     }
   }
 
@@ -81,6 +116,15 @@ function mouseClicked() {
     }
   }
 
+  if (ramaPos === 3 && decision && rama === 4) {
+    if (botonNo()) {
+      ramaPos = 0;
+      rama = 5;
+    } else if (botonSi()) {
+      ramaPos++;
+    }
+  }
+
   if (ramaPos === 5 && decision && rama === 5) {
     if (botonNo()) {
       ramaPos = 0;
@@ -92,54 +136,14 @@ function mouseClicked() {
     }
   }
 
-  if (ramaPos === 3 && decision && rama === 4) {
-    if (botonNo()) {
-      ramaPos = 0;
-      rama = 5;
-    } else if (botonSi()) {
-      ramaPos++;
-    }
-  }
-
-  if (decision && ramaPos === 3 && rama === 1) {
-    if (botonNo()) {
-      decision = false;
-      ramaPos = 0;
-      rama= 2;
-    } else if (botonSi()) {
-      ramaPos++;
-    }
-  }
-  
-  if (decision && ramaPos === 13 && rama === 0) {
-    if (botonNo()) {
-      rama = 7;
-      ramaPos = 0;
-      decision = false;
-    } else if (botonSi()) {
-      ramaPos++;
-      decision = false;
-    }
-  }
-
-  if (decision && ramaPos === 6 && rama === 0) {
-    if (botonNo()) {
-      decision = false;
-      ramaPos = 0;
-      rama = 1;
-    } else if (botonSi()) {
-      ramaPos++;
-    }
-  }
-
   if (mouseX > width - 75 && mouseX < width - 25 && mouseY > height - 75 && mouseY < height - 25 && siguiente) {
-  ramaPos++;
-}
+    ramaPos++;
+  }
 }
 function mousePressed() {
-  
+
   if (!musicaIniciada) {
-    sound.loop();  
-    musicaIniciada = true;  
+    sound.loop();
+    musicaIniciada = true;
   }
 }
